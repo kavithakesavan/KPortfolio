f9e181c489f54761ee2d1e595d48695628cfd12e
vbikavithak <kavithak@visualbi.com>
2025-06-17 12:59:26 +0530

    gh-pages commit

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
     console.log(event);
     document
       .getElementById(event.target.className)
-      .scrollIntoView({ behavior: "smooth" });
+      .scrollIntoView({ behavior: "smooth", block: "end" });
   };
   // const themeToggle= ()=>{
   //   console.log("theme",theme);
--- src/components/responsiblities.jsx
+++ src/components/responsiblities.jsx
@@ -16,14 +16,14 @@ export default function Responsiblities() {
   const listItems = listofresponsblities.map((item) => <li>{item}</li>);
   return (
     <div class="w-full min-h-screen justify-center items-center ">
-      <div class="flex flex-col justify-center items-center pb-5 md:py-5 px-4 md:px-10 md:py-10 gap-4">
+      <div class="flex flex-col justify-center items-center py-5 md:py-5 px-4 md:px-10 md:py-10 gap-4">
         <div class="text-4xl font-bold text-amber-500">Highlights</div>
         {/* <div class="text-xl text-center">
           Lumel (Formerly as Visual BI) ------------------ 2013-2023
           <br />
           Calsoft Labs(An Alten Company) ------------------ 2011-2013
         </div> */}
-        <div class="flex flex-col gap-4 py-10 px-5 md:px-7 rounded-lg border border-lime-800 hover:border-lime-600 duration-500 font-khula leading-relaxed text-lg font-semibold ">
+        <div class="flex flex-col gap-4 py-10 px-5 mt-8 md:px-7 rounded-lg border border-lime-800 hover:border-lime-600 duration-500 font-khula leading-relaxed text-lg font-semibold ">
           {listItems}
         </div>
       </div>
